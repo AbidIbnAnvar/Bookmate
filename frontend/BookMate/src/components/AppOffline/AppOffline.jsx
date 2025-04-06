e6787fa016dc26195f3463c8125412088ee19474
AbidIbnAnvar <abid_b220004cs@nitc.ac.in>
2025-04-06 15:44:47 +0530

    Added Admin panel and changed backend

---
+++ frontend/BookMate/src/components/AppOffline/AppOffline.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const AppOffline = () => {
+	return (
+		<>
+			<div className="bg-primary min-h-[80vh] flex flex-col items-center justify-center">
+				<h1 className="text-4xl font-bold text-secondary font-poppins mb-6">
+					Bookmate is Offline
+				</h1>
+			</div>
+		</>
+	);
+};
+
+export default AppOffline;
